7ab7ac248ad25a4856c53c0ce347d94aa8aabc05
nik <nik@MacBook-Air-nik.local>
2022-10-03 22:01:10 +0500

    решение задачи

--- task4/Program.cs
+++ task4/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход трехзнаяное число и на выходе показывает 
+// последнюю цифру этого числа.
+// 456 -> 6
+// 782 -> 2
+// 918 -> 8
+ 
+ Console.WriteLine("Введите число: ");
+ int number = int.Parse(Console.ReadLine());
+
+ Console.WriteLine(number % 10);
