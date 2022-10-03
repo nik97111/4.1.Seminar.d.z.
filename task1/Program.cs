d0900eb071e952263b55fb22ed64f75ccfaae9ba
nik <nik@MacBook-Air-nik.local>
2022-10-03 16:42:16 +0500

    решение задачи

---
+++ task1/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая на вход принимает два числа и провнеряет, 
+// является ли первое число квадратом второго.
+// a = 25, b = 5 -> да
+// a = 2, b = 10 -> нет
+// a = 9, b = -3 -> да
+// a = -3, b = 9 -> нет
+
+Console.Write("Введите число: ");
+int number1 = int.Parse(Console.ReadLine());
+int number2 = int.Parse(Console.ReadLine());
+
+if ((number1 / number2) == number2)
+{
+    Console.WriteLine("Первое число является корнем второго");
+}
+else 
+{
+    Console.WriteLine("Первое число не является корнем второго");
+}
