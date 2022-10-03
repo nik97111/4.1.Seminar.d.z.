eb3aba38dbe6fc1ce834477186f19d03fb561cf5
nik <nik@MacBook-Air-nik.local>
2022-10-04 00:07:45 +0500

    task 3

--- task3/Program.cs
+++ task3/Program.cs
@@ -2,3 +2,12 @@
 // все целые числа в промежутке от -N до N.
 // 4 -> " -4, -3, -2, -1, 0, 1, 2, 3, 4 "
 // 2 -> " -2, -1, 0, 1, 2 "
+
+Console.WriteLine("Введите число: ");
+ int N = int.Parse(Console.ReadLine());
+
+ int i = -N;
+ while (i <= N) {
+    Console.WriteLine(i + " , ");
+    i++;
+ }
