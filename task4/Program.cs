20ab45299f15ef45ff537c968dfba7f9b04e3f5f
nik <nik@MacBook-Air-nik.local>
2022-10-04 10:06:28 +0500

    Внесли изменения

--- task4/Program.cs
+++ task4/Program.cs
@@ -7,4 +7,8 @@
  Console.WriteLine("Введите число: ");
  int number = int.Parse(Console.ReadLine());
 
- Console.WriteLine(number % 10);
+ if (number > 10) {
+    Console.Write(number % 10);
+ } else {
+    Console.Write("Введите число, не менее двухзначного");
+ }
