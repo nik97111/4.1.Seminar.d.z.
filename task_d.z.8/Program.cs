45af9a0a69f0efd645d43b6f9e1dda8ef399d5e5
nik <nik@MacBook-Air-nik.local>
2022-10-04 16:33:16 +0500

    решение задачи

---
+++ task_d.z.8/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все четные 
+// числа от 1 до N
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.Write("Введите целое положительное число: ");
+int N = int.Parse(Console.ReadLine());
+
+int count = 0;
+while (count <= N) {
+    if ((count % 2) == 0) {
+    Console.WriteLine(count);
+    }
+    count++;
+}
+
+// int count = 0;
+// while (count <= N) {
+//     if ((count % 2) == 0) {
+//     Console.Write(count + ", ");
+//     }
+//     count++;
+// }
+
